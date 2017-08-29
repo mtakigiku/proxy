11c7a937f996b4f87243aa3ade2467b59142ff3d
mtakigiku <mtakigiku@google.com>
2017-08-29 23:37:28 +0000

    update setup.sh

--- src/envoy/auth/fortio_test_copy/setup.sh
+++ src/envoy/auth/fortio_test_copy/setup.sh
@@ -1,3 +1,3 @@
-envoy -c envoy_pkey_from_file.conf &
-envoy -c envoy_wo_filter.conf --base-id 1 &
+./envoy -c envoy_pkey_from_file.conf &
+./envoy -c envoy_wo_filter.conf --base-id 1 &
 go run hello_world.go &
